508f35f47c896c26249823efce0b749de0fc71e5
Florian Cavasin <florian@allsoftware.fr>
2018-07-09 10:44:09 +0200

    Add people

--- people/javascripts/app.js
+++ people/javascripts/app.js
@@ -17,71 +17,72 @@ $( document ).ready(function() {
             // birth_year	"19BBY"
             // gender	"male"
             // homeworld	"https://swapi.co/api/planets/1/"
+            // "films": [],
+            // "species": [],
+            // "vehicles": [],
+            // "starships": []
 
-            console.log(data.results);
+                console.log(data.results);
 
             $.each(data.results, function(index, value) {
+                console.log(index)
                 var gDiv = document.createElement( "div" );
-                gDiv.className = "ship col-md-4 col-md-offset-4";
+                gDiv.className = "people col-md-4 col-md-offset-4";
+
+                var divName = document.createElement( "div" );
+                divName.innerHTML = value.name;
+                divName.className = "people-name";
+                gDiv.append(divName);
+
+                var divGender = document.createElement( "div" );
+                divGender.innerHTML = "Gender : "+value.gender;
+                divGender.className = "people-gender";
+                gDiv.append(divGender);
 
                 var divHeight = document.createElement( "div" );
-                divHeight.innerHTML = value.height;
+                divHeight.innerHTML = "Height : "+value.height;
                 divHeight.className = "people-height";
                 gDiv.append(divHeight);
 
                 var divMass = document.createElement( "div" );
-                divMass.innerHTML = value.mass;
+                divMass.innerHTML = "Mass : "+value.mass;
                 divMass.className = "people-mass";
                 gDiv.append(divMass);
 
                 var divHair_color = document.createElement( "div" );
-                divHair_color.innerHTML = "Model : " + value.hair_color;
-                divHair_color.className = "ship-hair_color";
+                divHair_color.innerHTML = "Hair color : " + value.hair_color;
+                divHair_color.className = "people-hair_color";
                 gDiv.append(divHair_color);
 
                 var divSkin_color = document.createElement( "div" );
-                divSkin_color.innerHTML = "Model : " + value.skin_color;
-                divSkin_color.className = "ship-skin_color";
+                divSkin_color.innerHTML = "Skin color : " + value.skin_color;
+                divSkin_color.className = "people-skin_color";
                 gDiv.append(divSkin_color);
 
                 var divEye_color = document.createElement( "div" );
-                divEye_color.innerHTML = "Model : " + value.eye_color;
-                divEye_color.className = "ship-eye_color";
+                divEye_color.innerHTML = "Eye color : " + value.eye_color;
+                divEye_color.className = "people-eye_color";
                 gDiv.append(divEye_color);
 
-                var divEye_color = document.createElement( "div" );
-                divEye_color.innerHTML = "Model : " + value.eye_color;
-                divEye_color.className = "ship-eye_color";
-                gDiv.append(divEye_color);
+                var divBirth_year = document.createElement( "div" );
+                divBirth_year.innerHTML = "Birth year : " + value.birth_year;
+                divBirth_year.className = "people-birth_year";
+                gDiv.append(divBirth_year);
 
-                if (value.pilots.length > 0) {
-                    var divPilots = document.createElement( "div" );
-                    divPilots.innerHTML = "Pilots : </br>";
-                    divPilots.className = "ship-pilot-list";
-
-                    $.each(value.pilots, function(index, value) {
-                        var divPilot = document.createElement( "div" );
-
-                        var rest = value.slice(0,-1);
-                        var last = rest.substring(rest.lastIndexOf("/"), rest.length);
-                        last = last.substr(1);
-
-                        $.getJSON("https://swapi.co/api/people/"+last+"/?format=json", function(data) {
-
-                                divPilot.innerHTML = "<a href= ../people/" + last + ">" + data.name + "";
-                        });
-                        divPilot.className = "ship-pilot";
-
-                        divPilots.append(divPilot);
-                    });
-
-                    gDiv.append(divPilots);
-                }
+                var divHomeworld = document.createElement( "div" );
+                var rest = value.homeworld.slice(0,-1);
+                var last = rest.substring(rest.lastIndexOf("/"), rest.length);
+                last = last.substr(1);
+                $.getJSON("https://swapi.co/api/planets/"+last+"/?format=json", function(data) {
+                    divHomeworld.innerHTML = "Homeworld : <a href= ../planets/" + last + ">" + data.name + "";
+                });
+                divHomeworld.className = "people-homeworld";
+                gDiv.append(divHomeworld);
 
                 if (value.films.length > 0) {
                     var divFilms = document.createElement( "div" );
                     divFilms.innerHTML = "Films : </br>";
-                    divFilms.className = "ship-film-list";
+                    divFilms.className = "people-film-list";
 
                     $.each(value.films, function(index, value) {
                         var divFilm = document.createElement( "div" );
@@ -94,7 +95,7 @@ $( document ).ready(function() {
 
                             divFilm.innerHTML = "<a href= ../films/" + last + ">" + data.title + "";
                         });
-                        divFilm.className = "ship-film";
+                        divFilm.className = "people-film";
 
                         divFilms.append(divFilm);
                     });
@@ -102,7 +103,7 @@ $( document ).ready(function() {
                     gDiv.append(divFilms);
                 }
 
-                $(".starship-list").append(gDiv);
+                $(".people-list").append(gDiv);
             });
         });
 
