9b23a176f01c9482fdd83854c375d06f0e6f2dad
Florian Cavasin <florian@allsoftware.fr>
2018-07-10 08:56:05 +0200

    Vehicle

--- vehicles/javascripts/app.js
+++ vehicles/javascripts/app.js
@@ -2,81 +2,84 @@ $( document ).ready(function() {
 
         $('#header').load(blocksFolderLocation+"/header.html");
 
-        $.getJSON("https://swapi.co/api/starships/?format=json", function(data){
+        $.getJSON("https://swapi.co/api/vehicles/?format=json", function(data){
             // count: 37​
-            // next: https://swapi.co/api/starships/?page=2&format=json
+            // next: https://swapi.co/api/vehicles/?page=2&format=json
             // previous: null
             // results: Array(10)
 
-            // MGLT: "40"
-            // cargo_capacity: "250000000"
-            // consumables: "6 years"
-            // cost_in_credits: "1143350000"
-            // created: "2014-12-15T12:31:42.547000Z"
-            // crew: "279144"
-            // edited: "2017-04-19T10:56:06.685592Z"
-            // films: Array [ "https://swapi.co/api/films/2/", "https://swapi.co/api/films/3/" ]
-            // hyperdrive_rating: "2.0"
-            // length: "19000"
-            // manufacturer: "Kuat Drive Yards, Fondor Shipyards"
-            // max_atmosphering_speed: "n/a"
-            // model: "Executor-class star dreadnought"
-            // name: "Executor"
-            // passengers: "38000"
-            // pilots: Array []
-            // starship_class: "Star dreadnought"
-            // url: "https://swapi.co/api/starships/15/"
+            // "name": "Sand Crawler",
+            // "model": "Digger Crawler",
+            // "manufacturer": "Corellia Mining Corporation",
+            // "cost_in_credits": "150000",
+            // "length": "36.8",
+            // "max_atmosphering_speed": "30",
+            // "crew": "46",
+            // "passengers": "30",
+            // "cargo_capacity": "50000",
+            // "consumables": "2 months",
+            // "vehicle_class": "wheeled",
+            // "pilots": [],
+            // "films": [],
+            // "created": "2014-12-10T15:36:25.724000Z",
+            // "edited": "2014-12-22T18:21:15.523587Z",
+            // "url": "https://swapi.co/api/vehicles/4/"
 
             console.log(data.results);
 
             $.each(data.results, function(index, value) {
                 var gDiv = document.createElement( "div" );
-                gDiv.className = "ship col-md-4 col-md-offset-4";
+                gDiv.className = "vehicle col-md-4 col-md-offset-4";
 
                 var divName = document.createElement( "div" );
                 divName.innerHTML = value.name;
-                divName.className = "ship-name";
+                divName.className = "vehicle-name";
                 gDiv.append(divName);
 
                 var divModel = document.createElement( "div" );
                 divModel.innerHTML = "Model : " + value.model;
-                divModel.className = "ship-model";
+                divModel.className = "vehicle-model";
                 gDiv.append(divModel);
 
                 var divPrice = document.createElement( "div" );
                 divPrice.innerHTML = "Price : " + (value.cost_in_credits).toLocaleString('en-en') + " $";
-                divPrice.className = "ship-price";
+                divPrice.className = "vehicle-price";
                 gDiv.append(divPrice);
 
                 var divPassenger = document.createElement( "div" );
                 divPassenger.innerHTML = "Passenger max : " + value.passengers;
-                divPassenger.className = "ship-passenger";
+                divPassenger.className = "vehicle-passenger";
                 gDiv.append(divPassenger);
 
                 var divLenght = document.createElement( "div" );
                 divLenght.innerHTML = "Lenght : " + value.length;
-                divLenght.className = "ship-lenght";
+                divLenght.className = "vehicle-lenght";
                 gDiv.append(divLenght);
 
                 var divManufacturer = document.createElement( "div" );
                 divManufacturer.innerHTML = "Manufacturer : " + value.manufacturer;
-                divManufacturer.className = "ship-manufacturer";
+                divManufacturer.className = "vehicle-manufacturer";
                 gDiv.append(divManufacturer);
 
                 var divClass = document.createElement( "div" );
-                divClass.innerHTML = "Class : " + value.starship_class;
-                divClass.className = "ship-class";
+                divClass.innerHTML = "Class : " + value.vehicle_class;
+                divClass.className = "vehicle-vehicle_class";
                 gDiv.append(divClass);
+
+                var divCrew = document.createElement( "div" );
+                divCrew.innerHTML = "Crew : " + value.crew;
+                divCrew.className = "vehicle-crew";
+                gDiv.append(divCrew);
 
                 var divCargo = document.createElement( "div" );
                 divCargo.innerHTML = "Carego capacity : " + value.cargo_capacity;
-                divCargo.className = "ship-cargo";
+                divCargo.className = "vehicle-cargo";
                 gDiv.append(divCargo);
 
                 if (value.pilots.length > 0) {
                     var divPilots = document.createElement( "div" );
                     divPilots.innerHTML = "Pilots : </br>";
-                    divPilots.className = "ship-pilot-list";
+                    divPilots.className = "vehicle-pilot-list";
 
                     $.each(value.pilots, function(index, value) {
                         var divPilot = document.createElement( "div" );
@@ -89,7 +92,7 @@ $( document ).ready(function() {
 
                                 divPilot.innerHTML = "<a href= ../people/" + last + ">" + data.name + "";
                         });
-                        divPilot.className = "ship-pilot";
+                        divPilot.className = "vehicle-pilot";
 
                         divPilots.append(divPilot);
                     });
@@ -100,7 +103,7 @@ $( document ).ready(function() {
                 if (value.films.length > 0) {
                     var divFilms = document.createElement( "div" );
                     divFilms.innerHTML = "Films : </br>";
-                    divFilms.className = "ship-film-list";
+                    divFilms.className = "vehicle-film-list";
 
                     $.each(value.films, function(index, value) {
                         var divFilm = document.createElement( "div" );
@@ -113,7 +116,7 @@ $( document ).ready(function() {
 
                             divFilm.innerHTML = "<a href= ../films/" + last + ">" + data.title + "";
                         });
-                        divFilm.className = "ship-film";
+                        divFilm.className = "vehicle-film";
 
                         divFilms.append(divFilm);
                     });
@@ -121,7 +124,7 @@ $( document ).ready(function() {
                     gDiv.append(divFilms);
                 }
 
-                $(".starship-list").append(gDiv);
+                $(".vehicle-list").append(gDiv);
             });
         });
 
